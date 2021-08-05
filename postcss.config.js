940f9ee045fb5458c101d96c561020a55cf02e11
T.Watanabe <watanabe@colorfulcompany.co.jp>
2021-08-06 08:26:48 +0900

    add autoprefixer

--- postcss.config.js
+++ postcss.config.js
@@ -2,5 +2,8 @@ module.exports = {
   parser: 'postcss-scss',
   plugins: [
     require('csstools-postcss-sass-pre-release'),
+    require('postcss-preset-env')({
+      autoprefixer: true
+    })
   ]
 }
