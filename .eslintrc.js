8af7dc40afa5d2c51369a65186fdb2c8179eff12
cc-kawakami <kawakami-moeki@colorfulcompany.co.jp>
2022-02-02 14:28:14 +0900

    ESLint の shareable config を利用して、ESLint の設定は shareable config の方で一元管理する

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,34 +1,5 @@
 module.exports = {
-  env: {
-    es2017: true,
-    browser: true,
-    es2020: false,
-    es2021: false
-  },
-  parserOptions: {
-    ecmaVersion: 2017
-  },
-  extends: [
-    'standard',
-    'plugin:jsdoc/recommended'
-  ],
-  rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'jsdoc/require-jsdoc': [
-      'error',
-      {
-        require: {
-          ArrowFunctionExpression: true,
-          ClassDeclaration: true,
-          FunctionDeclaration: true,
-          MethodDefinition: true
-        }
-      }
-    ],
-    'jsdoc/require-param-description': 'off',
-    'jsdoc/require-returns-description': 'off'
-  },
+  extends: 'colorfulcompany/browser',
   ignorePatterns: [
     'vendor/**/*.js'
   ]
